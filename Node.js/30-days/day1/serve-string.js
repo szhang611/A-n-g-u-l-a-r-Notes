c1339e5c19ddc159a2884c136c255944b9534013
Sam H Zhang <szhang460@nam.ad.pwcinternal.com>
2018-04-09 14:21:13 +0800

    add 30-days-tutorial

---
+++ Node.js/30-days/day1/serve-string.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const host = '127.0.0.1';
+const port = 3000;
+
+let server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World!");
+    response.end();
+});
+
+server.listen(port,host, (error) => {
+    if (error) {
+        return console.log('Error occured : ', error );
+    }
+
+    console.log('server is listening on ' + host + ':'+ port);
+});
